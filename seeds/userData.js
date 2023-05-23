88ac71cc9c8e3956d3f5e2ebc723741fead318b8
Jen Wariner <jenniferwariner@yahoo.com>
2023-05-23 17:56:10 -0500

    created seeds

---
+++ seeds/userData.js
@@ -0,0 +1,24 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: 'Jen Wariner',
+        email: 'jenniferwariner@test.com',
+        password: 'Password1234',
+    },
+    {
+        username: 'Bill Hampton',
+        email: 'billhampton@test.com',
+        password: 'Password12',
+    },
+    {
+        username: 'Alexis Schitt',
+        email: 'alexisschitt@test.com',
+        password: 'Password34',
+    },
+
+];
+
+const seedUser = () => User.bulkCreate(userData);
+
+module.exports = seedUser;
